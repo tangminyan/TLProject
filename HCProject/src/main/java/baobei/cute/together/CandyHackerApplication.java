c388e7fb309e83e7034c8e545fa074965180307a
tangminyan <tangminyan@cmhi.chinamobile.com>
2019-03-11 16:36:45 +0800

    statemachine demo

--- HCProject/src/main/java/baobei/cute/together/CandyHackerApplication.java
+++ HCProject/src/main/java/baobei/cute/together/CandyHackerApplication.java
@@ -4,12 +4,16 @@ package baobei.cute.together;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Created by tangminyan on 2019/3/1.
  */
 @SpringBootApplication
-@EntityScan("baobei.cute.together")
+@EntityScan("baobei.cute")
+@ComponentScan(basePackages = {"baobei.cute"}) // controller service注解
+@EnableJpaRepositories("baobei.cute") //jpa
 public class CandyHackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CandyHackerApplication.class, args);
